891af3289f465a61ad2c5c1d5e8c7985bc50e648
toilatoiuy <toilatoiuy@gmail.com>
2026-01-29 20:20:55 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'xuan-lai-v1';
+const urlsToCache = ['/', '/index.html'];
+
+// Cài đặt và lưu các file vào bộ nhớ đệm
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Phản hồi dữ liệu khi mất mạng
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
