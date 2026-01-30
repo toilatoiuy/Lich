5536dea11af84d9ffcd538818e81d95622a5c2bf
toilatoiuy <toilatoiuy@gmail.com>
2026-01-30 09:31:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
-const CACHE_NAME = 'xuan-lai-v1';
+const CACHE_NAME = 'ios-calc-xuanlai-v1';
+const assets = ['./', './index.html', './manifest.json'];
 
-// Cài đặt: Lưu trữ mọi thứ vào bộ nhớ đệm
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html', './manifest.json'])));
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
 });
 
-// Kích hoạt: Xóa bộ nhớ đệm cũ nếu có cập nhật
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)))));
-});
-
-// Phản hồi: Ưu tiên lấy từ bộ nhớ đệm, nếu không có mới tải từ mạng
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
